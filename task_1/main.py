f20be6ccd5fcdc0a4fd2b914c544ee87dbca2d13
Belu <cryingsnowleopard12@gmail.com>
2025-12-18 07:57:13 +0300

    i forget the num of version

--- task_1/main.py
+++ task_1/main.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return 'For super Earth!'
 
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f"A new warrior({name}) for the Horde!"
+
+@app.route('/square/<int:number>')
+def square(number):
+    return str(number * number)
+
+@app.route('/calc')
+def calculator():
+    return render_template("html.html")
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
