9ee60a655ab1ad2180c119fbf5421d219ce18625
Belu <cryingsnowleopard12@gmail.com>
2025-12-19 07:59:30 +0300

    almost done

--- task_1/main.py
+++ task_1/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,9 +13,38 @@ def hello_name(name):
 def square(number):
     return str(number * number)
 
-@app.route('/calc')
+@app.route('/status')
+def json_status():
+    status = {
+    "status": "running",
+    "service": "Flask App"
+    }
+    return jsonify(status)
+
+@app.route('/math')
 def calculator():
+    operation= request.args.get('operation')
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+    match(operation):
+        case "+":
+            result = str(num1 + num2)
+            return result
+        case "-":
+            result = str(num1 - num2)
+            return result
+        case " /":
+            result = str(num1 / num2)
+            return result
+        case "*":
+            result = str(num1 * num2)
+            return result
+    return " "
+   
+
+@app.route('/calc')
+def render():
     return render_template("html.html")
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
