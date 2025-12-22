8ce606f7fc991d6b242f89d5f250f3acdc1dae84
Belu <cryingsnowleopard12@gmail.com>
2025-12-22 03:52:47 +0300

    need to be deleted

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
 
-
-@app.route('/Hello')
-def show_user_profile(username):
-    print ("ляляля")
-    return 'User %s' % username
-
-@app.route('/')
-def greetings():
-    return 'For Super Earth!'
-
-if __name__ == '__main__':
-    app.run()
